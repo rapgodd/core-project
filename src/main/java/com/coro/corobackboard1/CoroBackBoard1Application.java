291333fa0ffecada72d0cda3d06d9f9c57e56171
giyeon <bdcgy22@daum.net>
2024-05-13 19:12:19 +0900

    init

---
+++ src/main/java/com/coro/corobackboard1/CoroBackBoard1Application.java
@@ -0,0 +1,13 @@
+package com.coro.corobackboard1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoroBackBoard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoroBackBoard1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coro/corobackboard1/CoroBackBoard1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coro.corobackboard1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoroBackBoard1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
